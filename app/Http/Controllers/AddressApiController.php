eb350c80464e790dd8df27c6e1c1cd5d0a299fff
Hemraj Jibhau Solanki <siya21211@gmail.com>
2018-06-09 00:56:45 +0530

    address api

---
+++ app/Http/Controllers/AddressApiController.php
@@ -0,0 +1,72 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Address;
+
+use Illuminate\Http\Request;
+
+class AddressApiController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return Address::all();
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store(Request $request)
+  {
+      $address = Address::create($request->all());
+      return response()->json('Record inserted successfully', 201);
+  }
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		return Address::find($id);
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update(Request $request,$id)
+	{
+		$address = new Address;
+		$res = $address->update($request->all());
+    return response()->json('Record updated successfully', 200);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		$address = Address::find($id);
+		$address->delete();
+    return response()->json('Record Deleted Successfully', 204);
+	}
+
+}
